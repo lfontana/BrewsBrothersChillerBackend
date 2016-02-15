6b50cadb407d25b3057d6f7118316f79971fa278
Ryan Douglas <ryan@luckydogdigital.com>
2016-02-15 13:12:37 -0700

    basic route set up

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ require('dotenv').load();
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var batches = require('./routes/batches')
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/dashboard', batches)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

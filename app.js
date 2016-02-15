e07d3ef7c3efeeae15478f4184c06210bcfa22fb
Ryan Douglas <ryan@luckydogdigital.com>
2016-02-15 11:53:50 -0700

    server build out commit

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+require('dotenv').load();
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));

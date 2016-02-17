b8088ab4336e552779c688b487563a4124028867
David-Shibley <davidjshibley@gmail.com>
2016-02-17 12:39:23 -0700

    test

--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,14 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: {
+      user: process.env.PG_USERNAME,
+      password: process.env.PG_PASSWORD,
+      host: process.env.PG_HOST,
+      port: process.env.PG_PORT,
+      database: process.env.PG_DATABASE,
+      ssl: true
+    }
   }
 
 };

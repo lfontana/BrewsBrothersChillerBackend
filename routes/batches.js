e287cd9352d4a78df16e264e6cf457aa448ad87b
David-Shibley <davidjshibley@gmail.com>
2016-02-17 13:12:45 -0700

    fixed!

--- routes/batches.js
+++ routes/batches.js
@@ -14,7 +14,6 @@ router.get('/', function(req, res, next){
     console.log("data ", data);
     res.send(data);
   });
-  res.send(sampleData);
 })
 
 router.post('/', function(req, res){
@@ -24,30 +23,5 @@ router.post('/', function(req, res){
     name: req.body.name
   });
 });
-
-var sampleData = [
-  {
-    name: "Pale Ale - Default",
-    style: "pale ale",
-    created: 1454673600,
-    lastRun: 1454846400,
-    favorite: false,
-    schedule: [
-      {
-        time: 0,
-        temp: 68
-      },
-      {
-        time: 86400,
-        temp: 65
-      },
-      {
-        time: 604800,
-        temp: 50
-      }
-    ]
-  }
-
-]
 
 module.exports = router;

d281b26efa25727fe1962aeee08fe8208880601f
David-Shibley <davidjshibley@gmail.com>
2016-02-17 09:54:27 -0700

    added database connection for production

--- knexfile.js
+++ knexfile.js
@@ -5,56 +5,13 @@ require('dotenv').load();
 module.exports = {
 
   development: {
-  //   client: 'pg',
-  //   connection: {
-  //     user: process.env.PG_USERNAME,
-  //     password: process.env.PG_PASSWORD,
-  //     host: process.env.PG_HOST,
-  //     port: process.env.PG_PORT,
-  //     database: process.env.PG_DATABASE,
-  //     ssl: true
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-  //
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-  //
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  client: 'pg',
-  connection: 'postgres://localhost/my_db'
+    client: 'pg',
+    connection: 'postgres://localhost/my_db'
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL
   }
 
 };

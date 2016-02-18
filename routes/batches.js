c1bfba68345daa91a8c9e7fbdc6bc6801cad5c88
David-Shibley <davidjshibley@gmail.com>
2016-02-17 22:28:21 -0700

    save

--- routes/batches.js
+++ routes/batches.js
@@ -1,6 +1,10 @@
+require('dotenv').load();
+
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex');
+var db = require('mongodb');
+
 
 function Batches(){
   return knex('batches');
@@ -10,20 +14,43 @@ function Batches(){
 /* GET batches for dashboard */
 
 router.get('/', function(req, res, next){
-  Batches().select().then(function(data){
-    res.send(data);
-  });
+  Batches().select().where('user_id', rec.decoded.id).then(function(data) {
+    db.MongoClient.connect(process.env.MONGOLAB_URI, function(err, db){
+      var brews = db.collection('brews');
+      brews.find({
+        brew_id:data[0],
+        schedule: req.body.schedule
+      }, function(){
+        res.send(data);
+      })
+    });
+  };
 })
 
 router.post('/', function(req, res, next){
-  console.log("req body", req.body);
   Batches().insert({
     user_id: req.body.user_id,
     beer_id: req.body.styleNumber,
     name: req.body.name
   }, 'id').then(function(data){
-    res.end();
+    console.log(data);
+    db.MongoClient.connect(process.env.MONGOLAB_URI, function(err, db){
+      var brews = db.collection('brews');
+      brews.insert({
+        brew_id:data[0],
+        schedule: req.body.schedule
+      }, function(){
+        res.send("success");
+      })
+    });
   });
 });
+
+router.delete('/', function(req, res, next){
+  Batches().where('id', req.body.id).del()
+  .then(function(){
+    res.end();
+  })
+})
 
 module.exports = router;

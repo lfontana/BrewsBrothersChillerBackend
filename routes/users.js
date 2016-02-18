6e72997db1409d0adecd30e9560266a80e4b6b8f
Michael Yeager <m.yeager001@gmail.com>
2016-02-18 12:53:27 -0700

    add pi ip-adress

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
 
-
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated())
-    return next();
-  else
-    // Return error content: res.jsonp(...) or redirect: res.redirect('/login')
-    res.send('You need to log in !!!!')
-}
-
-// app.get('/account', ensureAuthenticated, function(req, res) {
-//   // Do something with user via req.user
-// });
 
 /* GET users listing. */
-router.get('/',ensureAuthenticated, function(req, res, next) {
-  res.send('respond with a resource');
+router.put('/pinumber', function(req, res, next) {
+  knex('users').where({email: req.user.email}).update('pi_id', req.body.piIpAddress).then(function(){
+    res.send('updated');
+  }).catch(function(err){
+    throw err;
+  })
 });
 
 module.exports = router;
